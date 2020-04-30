39c2d063e04383f63afa18fedd29c2251bc7a20e
Daniël le Roux <danielleroux25@gmail.com>
2020-04-30 16:27:21 +0200

    6_PB_Added Thruster Sounds and start stop play

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -6,11 +6,16 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        
     }
 
     // Update is called once per frame
@@ -23,17 +28,30 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+
             rigidBody.AddRelativeForce(Vector3.up);
             Debug.Log("Thrusting");
         }
+        else
+        {
+            audioSource.Stop();
+        }
+
+        
 
         if (Input.GetKey(KeyCode.A))
         {
+            transform.Rotate(Vector3.forward);            
             Debug.Log("Rotating Left");
         }
 
         else if (Input.GetKey(KeyCode.D))
         {
+            transform.Rotate(-Vector3.forward);
             Debug.Log("Rotating Right");
         }
     }
