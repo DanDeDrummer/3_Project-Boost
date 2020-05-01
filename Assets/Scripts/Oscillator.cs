ba780b6f6c96ab1f4e79b919658bcb900ebf0f8e
Daniël le Roux <danielleroux25@gmail.com>
2020-05-01 15:53:23 +0200

    6_PB_Added Oscillator Script to moving Obstacle with manual movement

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector;
+
+    //todo remove from insperctor
+    [SerializeField] [Range(0,1)]float movementFactor; //0 not move 1 to full move
+    //Start: 17.19 End: -4.3 Range:12.89
+
+    Vector3 startingPos;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
+    }
+}
